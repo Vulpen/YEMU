c75faca1967d12ecc4a807c851f0a62028b9fc5f
Thomas Lowe <musicaltommy@gmail.com>
2020-02-12 21:21:38 -0500

    Added Decode Stage and necessary register classes

--- PipelineRegisters.cs
+++ PipelineRegisters.cs
@@ -4,11 +4,178 @@ using System.Text;
 
 namespace Y86Emulator
 {
-    class PipelineRegisters
+    interface PipelineRegisters
     {
         //InitializeRegisters
-        //Dict<string, int64> Registers
         //ClearRegisters
+        //ReadRegister
+        //WriteRegister
+        public abstract void ClearRegisters();
+        public abstract Int64 ReadRegister(int reg);
+        public abstract bool WriteRegister(Int64 val, int reg);
+    }
 
+    enum RegisterFileRegisters
+    {
+        rax = 0,
+        rcx,
+        rdx,
+        rbx,
+        rsp,
+        rbp,
+        rsi,
+        rdi,
+        r8,
+        r9,
+        r10,
+        r11,
+        r12,
+        r13,
+        r14
+    }
+    class RegisterFile: PipelineRegisters
+    {
+        private static int NumberRegisters = 15;
+        private Int64[] Registers;
+
+        public void ClearRegisters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ReadRegister(int reg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WriteRegister(long val, int reg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    enum FRegisters
+    {
+    }
+    class FRegister : PipelineRegisters
+    {
+        private static int NumberRegisters = 0;
+        private Int64[] Registers;
+        public void ClearRegisters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ReadRegister(int reg)
+        {
+            if(reg < NumberRegisters)
+            {
+                return Registers[reg];
+            }
+            else
+            {
+                throw new Exception("Attempt to read invalid register");
+            }
+        }
+
+        public bool WriteRegister(long val, int reg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    enum DRegisters
+    {
+        stat = 0,
+        icode,
+        ifun,
+        rA,
+        rB,
+        valC,
+        valP
+    }
+    class DRegister : PipelineRegisters
+    {
+        private static int NumberRegisters = 7;
+        private Int64[] Registers;
+
+        public DRegister()
+        {
+            Registers = new long[NumberRegisters];
+        }
+
+        public void ClearRegisters()
+        {
+            for(int i = 0; i < NumberRegisters; i++)
+            {
+                Registers[i] = 0;
+            }
+        }
+
+        public long ReadRegister(int reg)
+        {
+            if(reg < NumberRegisters)
+            {
+                return Registers[reg];
+            }
+            else
+            {
+                throw new Exception("Attempt to read invalid register on Decode Register");
+            }
+        }
+
+        public bool WriteRegister(long val, int reg)
+        {
+            if (reg < NumberRegisters)
+            {
+                Registers[reg] = val;
+                return true;
+            }
+            else
+            {
+                throw new Exception("Attempt to read invalid register on Decode Register");
+            }
+        }
+    }
+
+    enum ERegisters
+    {
+        stat = 0,
+        icode,
+        ifun,
+        valC,
+        valA,
+        valB,
+        dstE,
+        dstM,
+        srcA,
+        srcB
+    }
+    class ERegister : PipelineRegisters
+    {
+        private static int NumberRegisters = 9;
+        private Int64[] Registers;
+
+        public ERegister()
+        {
+            Registers = new long[NumberRegisters];
+        }
+
+        public void ClearRegisters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long ReadRegister(int reg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WriteRegister(long val, int reg)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ProcessorStage.cs
+++ ProcessorStage.cs
@@ -4,7 +4,84 @@ using System.Text;
 
 namespace Y86Emulator
 {
-    class ProcessorStage
+    interface ProcessorStage
     {
+        public abstract bool Execute();
+
+        public abstract bool Reset();
+    }
+
+
+    class DecodeStage : ProcessorStage
+    {
+        //DRegister
+        DRegister InputRegisters;
+        //ERegister
+        ERegister OutputRegisters;
+        RegisterFile registerFile;
+
+        public DecodeStage(DRegister input, ERegister output, RegisterFile regFile)
+        {
+            InputRegisters = input;
+            OutputRegisters = output;
+            registerFile = regFile;
+        }
+        public bool Execute()
+        {
+            OutputRegisters.ClearRegisters();
+            switch (InputRegisters.ReadRegister((int)DRegisters.icode))
+            {
+                case (0):
+                    //halt
+                    throw new Exception("Halt Op");
+                    break;
+                case (1):
+                    //nop
+                    OutputRegisters.WriteRegister(1, (int)ERegisters.icode);
+                    return true;
+                    break;
+                case (2):
+                    //rrmovq rA, rB
+                    OutputRegisters.WriteRegister(2, (int)ERegisters.icode);
+                    OutputRegisters.WriteRegister(0, (int)ERegisters.ifun);
+                    
+                    OutputRegisters.WriteRegister(registerFile.ReadRegister((int)InputRegisters.ReadRegister((int)DRegisters.rA)), (int)ERegisters.valA);
+                    OutputRegisters.WriteRegister((int)InputRegisters.ReadRegister((int)DRegisters.rA), (int)ERegisters.srcA);
+                    return false;
+                case (3):
+                    //irmovq
+                    return false;
+                case (4):
+                    //rmmovq
+                    return false;
+                case (5):
+                    //mrmovq
+                    return false;
+                case (6):
+                    //OPq
+                    return false;
+                case (7):
+                    //jXX 
+                    return false;
+                case (8):
+                    //call Dest
+                    return false;
+                case (9):
+                    //ret
+                    return false;
+                case (10):
+                    //pushq rA
+                    return false;
+                case (11):
+                    //popq rA
+                    return false;
+            }
+            return false;
+        }
+
+        public bool Reset()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
