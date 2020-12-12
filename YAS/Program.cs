cda66047dccc39654fa91b5f4707ecdb2eca5ed6
Thomas Lowe <musicaltommy@gmail.com>
2020-12-12 14:22:49 -0500

    Adding functionality to shared YLib

--- YAS/Program.cs
+++ YAS/Program.cs
@@ -5,14 +5,12 @@ using YLib;
 
 namespace YAS
 {
-
-
     class Program
     {
-        static string PATH = @"E:\[]ProgrammingProjects\C#\YEMU\YAS\Examples\ex1.yas";
-        //static string PATH = @"D:\[KEEP]ProgrammingProjects\C#\Y86Emulator\YAS\Examples\ex1.yas";
-        //static string BIN_PATH = @"D:\[KEEP]ProgrammingProjects\C#\Y86Emulator\YAS\Examples\ex1.yin";
-        static string BIN_PATH = @"E:\[]ProgrammingProjects\C#\YEMU\YAS\Examples\ex1.yin";
+        //static string PATH = @"E:\[]ProgrammingProjects\C#\YEMU\YAS\Examples\ex1.yas";
+        static string PATH = @"D:\[KEEP]ProgrammingProjects\C#\Y86Emulator\YAS\Examples\ex1.yas";
+        static string BIN_PATH = @"D:\[KEEP]ProgrammingProjects\C#\Y86Emulator\YAS\Examples\ex1.yin";
+        //static string BIN_PATH = @"E:\[]ProgrammingProjects\C#\YEMU\YAS\Examples\ex1.yin";
         static void Main(string[] args)
         {
             Parser YParser = new Parser(EnumVerboseLevels.All);
--- YAS/TokenFile.cs
+++ YAS/TokenFile.cs
@@ -1,70 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using YLib;
 
 namespace YAS
 {
 
-    class InstructionSize
-    {
-        private Dictionary<EnumInstructions, int> Sizes;
-
-        public static Dictionary<EnumInstructions, int> AllSizes = new Dictionary<EnumInstructions, int>
-        {
-            {EnumInstructions.halt,  1},
-            {EnumInstructions.nop,   1},
-            {EnumInstructions.rrmov, 2},
-            {EnumInstructions.irmov, 10},
-            {EnumInstructions.rmmov, 10},
-            {EnumInstructions.mrmov, 10},
-            {EnumInstructions.ret,   1},
-            {EnumInstructions.push,  2},
-            {EnumInstructions.pop,   2},
-            {EnumInstructions.add,   2},
-            {EnumInstructions.sub,   2},
-            {EnumInstructions.imul,  2},
-            {EnumInstructions.xor,   2},
-            {EnumInstructions.jmp,   9},
-            {EnumInstructions.je,    9},
-            {EnumInstructions.jle,   9},
-            {EnumInstructions.jge,   9},
-            {EnumInstructions.jg,    9},
-            {EnumInstructions.jl,    9},
-        };
-
-        public InstructionSize()
-        {
-            Sizes = new Dictionary<EnumInstructions, int>();
-            Sizes.Add(EnumInstructions.halt, 1);
-            Sizes.Add(EnumInstructions.nop, 1);
-            Sizes.Add(EnumInstructions.rrmov, 2);
-            Sizes.Add(EnumInstructions.irmov, 10);
-            Sizes.Add(EnumInstructions.rmmov, 10);
-            Sizes.Add(EnumInstructions.mrmov, 10);
-            Sizes.Add(EnumInstructions.ret, 1);
-            Sizes.Add(EnumInstructions.push, 2);
-            Sizes.Add(EnumInstructions.pop, 2);
-            Sizes.Add(EnumInstructions.add, 2);
-            Sizes.Add(EnumInstructions.sub, 2);
-            Sizes.Add(EnumInstructions.imul, 2);
-            Sizes.Add(EnumInstructions.xor, 2);
-            Sizes.Add(EnumInstructions.jmp, 9);
-            Sizes.Add(EnumInstructions.je, 9);
-            Sizes.Add(EnumInstructions.jle, 9);
-            Sizes.Add(EnumInstructions.jge, 9);
-            Sizes.Add(EnumInstructions.jg, 9);
-            Sizes.Add(EnumInstructions.jl, 9);
-        }
-
-        public int GetInstructionSize(EnumInstructions inst)
-        {
-            if (Sizes.ContainsKey(inst))
-            {
-                return Sizes[inst];
-            }
-            throw new Exception("Tried to get an unsupported instruction size.");
-        }
-    }
+    
 
     class TokenLine
     {
@@ -122,7 +64,7 @@ namespace YAS
         //private List<Token[]> File;
         private List<TokenLine> File;
         private Int64 FileSize;
-        private InstructionSize Y86Sizes;
+        private InstructionSizes Y86Sizes;
         public int NumberOfLines
         {
             get { return File.Count; }
@@ -132,7 +74,7 @@ namespace YAS
         {
             File = new List<TokenLine>();
             LabelTable = new Dictionary<string, long>();
-            Y86Sizes = new InstructionSize();
+            Y86Sizes = new InstructionSizes();
             FileSize = 0;
         }
 
