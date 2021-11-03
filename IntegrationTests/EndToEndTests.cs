551e6a9a2ae0cc1c58a52ff6012b199035fccc52
Thomas Lowe <musicaltommy@gmail.com>
2021-11-03 08:04:24 -0400

    Added end-to-end tests in the unit test suite.

---
+++ IntegrationTests/EndToEndTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using YAS;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class EndToEndTests
+    {
+        [TestMethod]
+        public void TestExampleOne()
+        {
+            Assembler myAssembler = new Assembler();
+            string workingDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(workingDirectory);
+
+            Assert.IsTrue(myAssembler.AssembleFile(workingDirectory + @"\Examples\ex1.yas", workingDirectory + @"\Examples\ex1.yin"));
+        }
+
+    }
+}
